61625e037505a41d0beb90bd79aeb778b5139bea
koles77 <koles.sergy@yandex.ru>
2026-02-16 15:23:48 +0300

    Initial commit: Spring Boot + Docker + K8s + CI

---
+++ src/main/java/org/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package org.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
